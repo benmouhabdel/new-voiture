9ccae468a0340670927fa1d8febbed7d20a32b42
Zeinot <zeinotgaming@gmail.com>
2024-12-24 15:57:32 +0100

    added whatsapp btn

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -7,7 +7,7 @@
         <div class="p-6">
             <div class="flex justify-between items-center mb-4">
                 <h1 class="text-2xl font-bold">Liste des Voitures</h1>
-                <button type="button" class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter Voiture</button>
+                <a href="{{route('cars.create')}}" class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter Voiture</a>
             </div>
 
             <!-- Search and Filters Section -->
--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,22 @@ Route::get('/s/{car}', function (Car $car) {
     return view('show-car', compact('car'));
 });
 
-
+Route::middleware('auth')->group(function () {
 // Routes pour les voitures (CRUD complet)
-Route::resource('cars', CarController::class);
+    Route::resource('cars', CarController::class);
 
 // Routes pour les réservations (CRUD complet)
-Route::resource('reservations', ReservationController::class);
+    Route::resource('reservations', ReservationController::class);
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
